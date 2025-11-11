f958d2de710daca520c9e4dbe1f55dc4b5d5f438
quigerdestroyer <matematban@gmail.com>
2025-11-12 01:35:50 +0300

    Add files via upload

---
+++ pas.js
@@ -0,0 +1,14 @@
+function find_edit(){
+    document.getElementById('GivenBy').innerHTML="Given By: The Ministry of Internal Affairs of the Moscow region" //убрал ненужную переменную
+
+    document.getElementById('Country').innerHTML="Russian Federation" //убрал ненужную переменную
+
+    document.getElementById("name").innerHTML="Egor"
+
+    document.getElementById("surname").innerHTML="Novozhilov"
+
+    document.getElementById("date").innerHTML="10.19.2006"
+}
+
+const node_for_click = document.getElementById("for_click")
+node_for_click.addEventListener("click",find_edit)
